c39ef054c73bc685c452a18d54b680c0518a9925
Mark Hingston <9764437+mhingston@users.noreply.github.com>
2026-09-20 13:56:16 +0100

    refactor: stop exporting local decision client

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export * from "./context.js";
 export * from "./toolRouter.js";
 export * from "./postAction.js";
 export * from "./evaluation.js";
-export * from "./decision.js";
 export * from "./classification.js";
 export * from "./research.js";
 export * from "./researchRunner.js";
